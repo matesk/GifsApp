5d6b263685c75c02f894a61695995271e8719525
Pablo Matilla <matillapablo@gmail.com>
2021-03-04 10:51:09 +0100

    Add project GifsApp

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = 'Ic2ULRPHyj1k11vFXaBWxfY8h4u7lvA8';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
+    // if (localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+
+  }
+
+
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', query)
+      .set('limit', '10');
+
+    this.http.get<SearchGifsResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(response => {
+        this.resultados = response.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
+
+  }
+}
+
